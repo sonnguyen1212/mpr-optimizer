fa8f80a83cf054ce4c4f139fd08008ccf0154501
lablabla <lablabla@gmail.com>
2012-12-07 17:05:55 +0200

    addOffsetToOperation Method

--- Mpr Optimizer/src/mpr/mprWriter.java
+++ Mpr Optimizer/src/mpr/mprWriter.java
@@ -4,39 +4,46 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class mprWriter {
-	//empty constructor
+	// empty constructor
 	public static final String PLATE_DIRECTORY = "Nesting_Mprs";
 	public static final String LEFT_OVER = "LeftOvers_Mprs";
+	public static Pattern mprLine = Pattern.compile("(\\w)=(\"\\w\")");
+	public static final String[] xParameters = { "XA", "XE" };
+	public static final String[] yParameters = { "YA", "YE" };
 
-	//search recursively the given file name in the directory, if found return a File object of it,
-	//if not - return NULL;
-	public static File findFile(String fileName, String sourceDirectory)
-	{
+	// search recursively the given file name in the directory, if found return
+	// a File object of it,
+	// if not - return NULL;
+	public static File findFile(String fileName, String sourceDirectory) {
 		File sourceDir = new File(sourceDirectory);
-		if(!sourceDir.isDirectory()) {
+		if (!sourceDir.isDirectory()) {
 			return null;
 		}
 		File[] fileList = sourceDir.listFiles();
-		for(File file : fileList) {
-			if(file.isDirectory()) {
+		for (File file : fileList) {
+			if (file.isDirectory()) {
 				File nestedFile = findFile(fileName, file.getAbsolutePath());
-				if(nestedFile != null) {
+				if (nestedFile != null) {
 					return nestedFile;
 				}
 			}
-			if(file.getName().equals(fileName)) {
+			if (file.getName().equals(fileName)) {
 				return file;
 			}
 		}
 		return null;
 	}
 
-	//this method will recieve the dimensios of the plate, and the lines to add to it, it will generate
-	//the header of the document and concat it to the lines given and will write the file to the PLATE_DIRECFORY
-	public static void createPlateMpr(Point3D dimensions, ArrayList<String> lines, String name){
+	// this method will recieve the dimensios of the plate, and the lines to add
+	// to it, it will generate
+	// the header of the document and concat it to the lines given and will
+	// write the file to the PLATE_DIRECFORY
+	public static void createPlateMpr(Point3D dimensions,
+			ArrayList<String> lines, String name) {
 		ArrayList<String> headerLines = new ArrayList<>();
 		headerLines.add("[H\n\r");
 		headerLines.add("VERSION=\"4.0\"\n\r");
@@ -49,29 +56,30 @@ public class mprWriter {
 		headerLines.add("FNY=\"0\"\n\r");
 		headerLines.add("AX=\"0\"n\r");
 		headerLines.add("AY=\"0\"\n\r");
-		for(String line: lines) {
+		for (String line : lines) {
 			headerLines.add(line);
 		}
 		File plateDir = new File(PLATE_DIRECTORY);
-		if(!plateDir.exists()) {
+		if (!plateDir.exists()) {
 			plateDir.mkdirs();
 		}
 		fileWriter(PLATE_DIRECTORY + File.separator + name, headerLines);
 	}
 
-	//this method will recieve all the neccaserry lines for the this mpr, and will write it to the
-	//LEFT_OVER directory.
-	public static void createLeftOverMpr(ArrayList<String> lines, String name){
+	// this method will recieve all the neccaserry lines for the this mpr, and
+	// will write it to the
+	// LEFT_OVER directory.
+	public static void createLeftOverMpr(ArrayList<String> lines, String name) {
 		File leftOverDir = new File(LEFT_OVER);
-		if(!leftOverDir.exists()) {
+		if (!leftOverDir.exists()) {
 			leftOverDir.mkdirs();
 		}
 		fileWriter(LEFT_OVER + File.separator + name, lines);
 	}
 
-	//this method will be used by the two methods above, it will open the stream and write the files.
-	private static void fileWriter (String destination , ArrayList<String> lines)
-	{
+	// this method will be used by the two methods above, it will open the
+	// stream and write the files.
+	private static void fileWriter(String destination, ArrayList<String> lines) {
 		File destinationFile = new File(destination);
 		PrintWriter printWriter = null;
 		try {
@@ -84,17 +92,29 @@ public class mprWriter {
 				printWriter.println(line);
 			}
 		}
-		if(printWriter != null) {
+		if (printWriter != null) {
 			printWriter.close();
 		}
 	}
 
-	public Pattern mprLine = Pattern.compile("(\\w)=(\\w)");
-	public static final String[] xParameters = {"XA" , "XE"};
-	public static final String[] yParameters = {"YA" , "YE"};
-
-	public static void addOffsetToOperation (ArrayList<String> lines , String xOffset, String yOffset)
-	{
-
+	public static void addOffsetToOperation(ArrayList<String> lines,
+			String xOffset, String yOffset) {
+		for (String line : lines) {
+			Matcher mprMatcher = mprLine.matcher(line);
+			if(mprMatcher.find()) {
+				for(int i=0;i<xParameters.length;i++) {
+					if(mprMatcher.group(NestingCreator.PARAMETER_NAME).equals(xParameters[i])) {
+						line = xParameters[i] + "\"" + mprMatcher.group(NestingCreator.PARAMETER_VALUE) + xOffset + "\"\n\r";
+						break;
+					}
+				}
+				for(int i=0;i<yParameters.length;i++) {
+					if(mprMatcher.group(NestingCreator.PARAMETER_NAME).equals(yParameters[i])) {
+						line = yParameters[i] + "\"" + mprMatcher.group(NestingCreator.PARAMETER_VALUE) + yOffset + "\"\n\r";
+						break;
+					}
+				}
+			}
+		}
 	}
 }
